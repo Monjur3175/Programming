08539bc31fdfa8c8961719eac3735950b2796049
Monjur3175 <xavieritsolution@gmail.com>
2024-12-30 09:09:31 +0600

    30 December,2024

---
+++ C Programming Practice/A_Say_Hello_With_C.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char x[12];
+    scanf("%s",x);
+    printf("Hello, %s");
+    return 0;
+}
---
+++ C Programming Practice/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char x[10];
+    printf("Enter your name:");
+    scanf("%s",x);
+    printf("Hello %s\n");
+    return 0;
+}
